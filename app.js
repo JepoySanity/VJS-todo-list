6ed7185be56b5e2664a96cb100a863a1a870b946
Jepoy Factoran <factoran20@gmail.com>
2022-04-27 17:09:27 +0800

    creating ui and some js functionalities

---
+++ app.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", function () {
+  let taskForm = document.querySelector("#task-form");
+  let taskInput = document.querySelector(".task-input");
+  let tasks = document.querySelector(".tasks");
+
+  taskForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (!taskInput.value) {
+      alert("required input");
+    } else {
+      const taskEl = document.createElement("div");
+      taskEl.className = "task";
+      const taskText = document.createElement("input");
+      taskText.className = "task-content";
+      taskText.value = taskInput.value;
+
+      taskEl.appendChild(taskText);
+      tasks.appendChild(taskEl);
+
+      taskInput.value = "";
+      taskText.setAttribute("readonly", "readonly");
+
+      taskInput.focus();
+    }
+  });
+});
